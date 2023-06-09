ce2a0cd72b9dabaf62d7f91677dd37fbe776d693
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-06-09 11:50:51 -0500

    setting up index view

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const app = express();
 const port = 3000;
 const drinks = require('./database');
 
-app.get('/drinks', (req, res) => {
-  res.json(drinks);
-});
-  
-
 app.get('/', (req, res) => {
     res.send('Welcome to the Gitpub App!');
   });
 
+  app.set('view engine', 'ejs');
+  app.get('/drinks', (req, res) => {
+    res.render('index', { drinks: drinks });
+  });
+  
 
 
 
